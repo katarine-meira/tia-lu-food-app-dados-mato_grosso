404c7bd117c2c1157ba5f32b460555cfcd1ccb39
Katarine <katarine.kmo@gmail.com>
2025-09-20 16:33:23 -0300

    arquivo py criado

---
+++ tia-li-food-app-mato_grosso.py
@@ -0,0 +1,11 @@
+def menu():
+    menu = """
+    =================== MENU ===================
+    [1] \tCadastrar Item 
+    [2] \tAtualizar Item
+    [3] \tConsultar Itens
+    [4] \tDetalhes do Item
+    [0] \tSair
+
+    => """
+    return input(menu)
