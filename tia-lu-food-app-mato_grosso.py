d962229c236da7f3e40fd6789b908a86701554a2
Katarine <katarine.kmo@gmail.com>
2025-09-26 20:36:17 -0300

    criado relatorio de vendas

--- tia-lu-food-app-mato_grosso.py
+++ tia-lu-food-app-mato_grosso.py
@@ -15,6 +15,17 @@ def sair():
     
     return input(escolhaSair)
 
+#fução reutilizavel de escolha
+def pergunta(val):
+    opcao = f"""
+    ============= ESCOLHA ===============
+    
+    [1] \t{val}
+    [0] \tSair
+    
+    => """
+    
+    return input(opcao)
 
 # MENUS ->>>
 
@@ -43,6 +54,18 @@ def menuItens():
     => """
     return input(menu)
 
+def menuConsultas():
+    menu = """
+    ================== MENU CONSULTAS ==================
+    
+    [1] \tExibir Pedidos 
+    [2] \tFiltro por Status
+    [3] \tRelatório de Vendas
+    [0] \tSair
+
+    => """
+    return input(menu)
+
 def menuPedidos():
     menu = """
     ================= MENU PEDIDOS =================
@@ -51,11 +74,55 @@ def menuPedidos():
     [2] \tProcessar Pedidos Pendentes
     [3] \tAtualizar Status de Pedido
     [4] \tCancelar Pedido
+    [5] \tConsultas
     [0] \tSair
 
     => """
     return input(menu)
 
+# FUNÇÃO DE CRIAR CLIENTE ->>>
+
+clientes = []
+def criarCliente():
+    if clientes:
+        print("\nEscolha o usuário\n")
+        for i, cliente in enumerate(clientes):
+            print(f"[{i}] - {cliente}")
+        print(f"[{len(clientes)}] - Criar novo cliente")  # opção extra
+
+        try:
+            opcao = int(input("\n>>: "))
+        except ValueError:
+            print("Digite um número válido!")
+            return
+
+        # Se escolheu cliente existente
+        if 0 <= opcao < len(clientes):
+            print(f"Bem-vindo {clientes[opcao]}!")
+        
+        # Se escolheu criar novo
+        elif opcao == len(clientes):
+            nome = input("Digite seu nome de usuário: ")
+            clientes.append(nome)
+            print(f"Bem-vindo {nome}!")
+        
+        else:
+            print("Opção inválida!")
+
+    else:
+        print("\nAinda não existem clientes cadastrados.")
+        menu = pergunta("Criar cliente")
+        while True:
+            match menu:
+                case '1':
+                    nome = input("\nDigite seu nome de usuário: ")
+                    clientes.append(nome)
+                    print(f"Bem vindo {clientes[0]}!")
+                    return
+                case '0':
+                    return
+                case _:
+                        print("Opção inválida")
 
 # FUNÇÕES DE MENU ITENS ->>>
 
@@ -94,12 +161,12 @@ def cadastrarItem():
         except ValueError:
             print("Digite o valor usando '.'(ponto)")
             continue
-    estoque = (input("Digite a quantidade do estoque: "))
+    estoque = int(input("Digite a quantidade do estoque: "))
     
     numero = int(uuid.uuid4().int %100000)
     codigo = f"PRO{numero:04d}"
     
-    item = (nome, descricao, codigo, preco, estoque)
+    item = [nome, descricao, codigo, preco, estoque]  # <-- lista
     itemCadastrado.append(item)
     
     print("\nProduto cadastrado com sucesso!\n")
@@ -116,7 +183,7 @@ def atualizarItens():
         print(f"[{i}] {item[0]} (R${item[3]} - estoque: {item[4]})")
 
     try: 
-        indice = int(input("\nDigite o número do item que será atualizado:"))
+        indice = int(input("\nDigite o número do item que será atualizado: "))
         if indice < 0 or indice >= len(itemCadastrado):
             print("Item inválido.")
             return
@@ -134,7 +201,7 @@ def atualizarItens():
     estoque_atual = input(f"Estoque atual [{estoque}]:") or estoque
 
     # ITENS ATUALIZADOS
-    itemCadastrado[indice]= (nome_atual, descricao_atual, codigo, preco_atual, estoque_atual)
+    itemCadastrado[indice]= [nome_atual, descricao_atual, codigo, preco_atual, estoque_atual]
 
     print("\nItens atualizados!")
 
@@ -156,7 +223,7 @@ def detalhesItens():
             print(f"Nome: {item[0]}")
             print(f"Descrição: {item[1]}")
             print(f"Código: {item[2]}")
-            print(f"Preço: R$ {float(item[3].replace(',', '.')):.2f}")  #vou tirar o replace depois -kat
+            print(f"Preço: R$ {item[3]:.2f}")
             print(f"Estoque: {item[4]}\n")           
     else:
         print("\nNenhum item cadastrado.")
@@ -164,16 +231,6 @@ def detalhesItens():
 
 # FUNÇÕES DE MENU PEDIDOS ->>>
 
-def adicionarPedido():
-    escolhaPedido = """
-    ============= ESCOLHA ===============
-    
-    [1] \tAdicionar mais produtos
-    [0] \tSair
-    
-    => """
-    
-    return input(escolhaPedido)
 
 pedidosPendentes = []
 
@@ -194,31 +251,35 @@ def criarPedido():
                 continue
             
             if 0 <= indice < len(itemCadastrado):
-                pedido = {
-                    "nome": itemCadastrado[indice][0],
-                    # "descricao": itemCadastrado[indice][1],
-                    "codigo": itemCadastrado[indice][2],
-                    "preco": itemCadastrado[indice][3],
-                    # "estoque": itemCadastrado[indice][4],
-                }
-                pedido_usuario["produtos"].append(pedido)
+                # verifica o estoque
+                if itemCadastrado[indice][4] > 0:
+                    # decrementa do estoque
+                    itemCadastrado[indice][4] -= 1
+                    pedido = {
+                        "nome": itemCadastrado[indice][0],
+                        "codigo": itemCadastrado[indice][2],
+                        "preco": itemCadastrado[indice][3],
+                    }
+                    pedido_usuario["produtos"].append(pedido)
 
-                print("\nSua lista atual de pedidos:")
-                for p in pedido_usuario["produtos"]:
-                    print(f"- {p['nome']}")
-
-                controle = True
-                while controle == True:
-    
-                    match adicionarPedido():
-                        case '1':
-                            consultarItens()
-                            controle = False
-                        case '0':
-                            qtd_produto = False
-                            controle = False 
-                        case _:
-                            print("\nOpção inválida")
+                    print("\nSua lista atual de pedidos:")
+                    for p in pedido_usuario["produtos"]:
+                        print(f"- {p['nome']}")
+                        
+                    controle = True
+                    while controle == True:
+        
+                        match pergunta("Adicionar mais produtos"):
+                            case '1':
+                                consultarItens()
+                                controle = False
+                            case '0':
+                                qtd_produto = False
+                                controle = False 
+                            case _:
+                                print("\nOpção inválida")
+                else:
+                    print(f"\n{itemCadastrado[indice][0]} está sem estoque.")
             else:
                 print("\nÍndice inválido.")
                 controle2=True
@@ -262,19 +323,22 @@ def ProcessarPedidos():
                 valor_total = 0
                 for produto in pedido['produtos']:
                     valor_total = valor_total + produto['preco']
+                valor_final_pago = valor_total
                 while True:
                     try:
-                        cupom = input("Deseja utilizar o cupom de desconto do dia?[s ou n]: ")
+                        cupom = input("Deseja utilizar o cupom de desconto do dia?[s ou n]: ").lower()
                         break
                     except ValueError:
                         print("Digite 's' para sim, ou 'n' para não.")
                         continue
                 if(cupom == 's'):
                     desconto = valor_total/10
-                    valor_final = valor_total - desconto
-                    print(f"\nValor total pago: {valor_final}")
+                    valor_final_pago = valor_total - desconto
+                    print(f"\nValor total pago: {valor_final_pago:.2f}")
                 else:
-                    print(f"\nValor total pago: {valor_total}")
+                    print(f"\nValor total pago: {valor_total:.2f}")
+                    valor_final_pago = valor_total
+                pedido['valor_final_pago'] = valor_final_pago
                 pedido['status'] = "Aceito"
                 linha = pedidosPendentes.pop(0)
                 filaPreparo.append(linha)
@@ -304,7 +368,7 @@ def atualizarStatusPedido():
         print(f"ID do Pedido - {pedido['id_pedido']} (Status: {pedido['status']})")
         for produto in pedido['produtos']:
             print(f"   - {produto['nome']}")
-        print()
+        print("______________________________________________")
 
     if not filaPreparo:
         print("NENHUM PEDIDO A SER ATUALIZADO!")
@@ -368,6 +432,19 @@ def cancelarPedido():
     filaCancelados.append(pedido)
     print("\nPedido cancelado com sucesso!")
 
+def exibirPedidos():
+    print("oi")
+def filtroStatus():
+    print("oi")
+
+def relatorioVendas():
+    totalFaturamento = 0
+    for pedido in filaPreparo:
+        if pedido['status'] in ("Aceito", "Em preparo", "Pedido pronto!", "Aguardando o entregador!", "Seu pedido saiu para entrega!", "Pedido entregue!"):
+            totalFaturamento += pedido['valor_final_pago']
+    print("\n===== RELATÓRIO DE FATURAMENTO =====")
+    print(f"\nFaturamento total: R$ {totalFaturamento:.2f}")
+
 # LOOPING DO SISTEMA ->>>
 
 controle = True
@@ -397,13 +474,33 @@ while controle == True:
                 escolha = menuPedidos()
                 match escolha:
                     case '1':
+                        criarCliente()
                         criarPedido()
                     case '2':
+                        criarCliente()
                         ProcessarPedidos()
                     case '3':
+                        criarCliente()
                         atualizarStatusPedido()
                     case '4':
+                        criarCliente()
                         cancelarPedido()
+                    case '5':
+                        criarCliente()
+                        print(filaPreparo)
+                        while True:
+                            escolha = menuConsultas()
+                            match escolha:
+                                case '1':
+                                    exibirPedidos()
+                                case '2':
+                                    filtroStatus()
+                                case'3':
+                                    relatorioVendas()
+                                case '0':
+                                    break
+                                case _:
+                                    print("Opção inválida")
                     case '0':
                         break
                     case _:
@@ -412,5 +509,3 @@ while controle == True:
             controle = False
         case _:
             print("Opção inválida")
-
-    
